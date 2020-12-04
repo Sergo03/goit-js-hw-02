f03d03eaca790b6b7cf72833ce4a68276ae73a6c
Sergey Karpynskyi <karpinskiy94@gmail.com>
2020-12-04 16:46:58 +0200

    hw2

---
+++ js/task-1.js
@@ -0,0 +1,16 @@
+
+const logItems = function (item) {
+    
+for (let i = 0; i < item.length; i += 1) {
+   
+    console.log(`${i + 1} - ${item[i]}`); 
+
+}
+}
+
+
+
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
